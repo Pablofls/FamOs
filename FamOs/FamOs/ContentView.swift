35552ad83043ece65d26e231ed171be74df8a8d6
Pablofls <pablo.flsrdz@gmail.com>
2026-04-08 11:54:41 -0600

    add estructura swift

---
+++ FamOs/FamOs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FamOs
+//
+//  Created by Pablo Flores on 08/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FamOs/FamOs/FamOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamOsApp.swift
+//  FamOs
+//
+//  Created by Pablo Flores on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FamOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
